1bb6802beb48f5c8186171a1786bc91eafb5d383
Jadeja Mayurdhwajsinh <mayur.jadeja-i@equestsolutions.net>
2026-03-24 11:49:52 +0530

    footer created

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Logo from "../../assets/Logo.png";
-import twitter from "../../assets/twitter.svg"
-import facebook from "../../assets/facebook.svg"
-import instagram from "../../assets/instagram.svg"
+import socials from "../../assets/Social Media.svg"
 import footerImg1 from "../../assets/footer-img1.svg"
 import footerImg2 from "../../assets/footer-img2.svg"
 import footerImg3 from "../../assets/footer-img3.svg"
@@ -16,7 +14,6 @@ function Footer() {
 
                 <div className="footer__top">
 
-                    {/* LEFT */}
                     <div className='footer__left'>
                         <img
                             src={Logo}
@@ -29,17 +26,14 @@ function Footer() {
                         </p>
 
                         <div className="footer__socials">
-                            <img src={twitter} alt='Twitter' className='footer__social-icon' />
-                            <img src={facebook} alt='Facebook' className='footer__social-icon' />
-                            <img src={instagram} alt='Instagram' className='footer__social-icon' />
+                           <a href='#'> <img src={socials} alt='Twitter' /> </a>
                         </div>
                     </div>
 
-                    {/* RIGHT */}
                     <div className='footer__right'>
 
                         <ul className="footer__links">
-                            <li className="footer__item"><a href='#' className="footer__link">Product</a></li>
+                            <li className="footer__item"><a href='#' className="footer__link head">PRODUCT</a></li>
                             <li className="footer__item"><a href='#' className="footer__link">Testimonials</a></li>
                             <li className="footer__item"><a href='#' className="footer__link">Pricing</a></li>
                             <li className="footer__item"><a href='#' className="footer__link">Features</a></li>
@@ -47,7 +41,7 @@ function Footer() {
                         </ul>
 
                         <ul className="footer__links">
-                            <li className="footer__item"><a href='#' className="footer__link">About Kozyfull</a></li>
+                            <li className="footer__item"><a href='#' className="footer__link head">ABOUT KOZYFULL</a></li>
                             <li className="footer__item"><a href='#' className="footer__link">Our team</a></li>
                             <li className="footer__item"><a href='#' className="footer__link">Careers</a></li>
                             <li className="footer__item"><a href='#' className="footer__link">Press</a></li>
@@ -55,7 +49,7 @@ function Footer() {
                         </ul>
 
                         <ul className="footer__links">
-                            <li className="footer__item"><a href='#' className="footer__link">Contact</a></li>
+                            <li className="footer__item"><a href='#' className="footer__link head">CONTACT</a></li>
                             <li className="footer__item"><a href='mailto:info@site.com' className="footer__link">info@site.com</a></li>
                             <li className="footer__item footer__text">882-587-3025</li>
                             <li className="footer__item footer__text">6116 Willa River Suite 610</li>
@@ -65,24 +59,21 @@ function Footer() {
                 </div>
 
                 <div className="footer__center">
-                    <div>
+                    <div className="footer__center-item">
                         <img src={footerImg1} alt="" />
                         <p>Fast shipping</p>
                     </div>
-                    <div>
+                    <div className="footer__center-item">
                         <img src={footerImg2} alt="" />
                         <p>24/7 customer support</p>
-
                     </div>
-                    <div>
+                    <div className="footer__center-item">
                         <img src={footerImg3} alt="" />
                         <p>Top quality materials</p>
-
                     </div>
-                    <div>
+                    <div className="footer__center-item">
                         <img src={footerImg4} alt="" />
                         <p>100% Money-back guarantee</p>
-
                     </div>
                 </div>
 
